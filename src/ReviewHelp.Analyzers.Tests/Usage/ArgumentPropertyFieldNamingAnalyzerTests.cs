02883c887955fd42ab4473ebd5750f60bb19ef82
jokokko <jokokko@gmail.com>
2018-04-03 12:49:35 +0300

    Analyzer for ctor argument naming

---
+++ src/ReviewHelp.Analyzers.Tests/Usage/ArgumentPropertyFieldNamingAnalyzerTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using ReviewHelp.Analyzers.Tests.Infrastructure;
+using ReviewHelp.Analyzers.Usage;
+using Xunit;
+
+namespace ReviewHelp.Analyzers.Tests.Usage
+{
+    public sealed class ArgumentPropertyFieldNamingAnalyzerTests
+    {
+        private readonly DiagnosticAnalyzer analyzer = new ArgumentPropertyFieldNamingAnalyzer();
+
+        [Fact]
+        public async void CanIdentifyNameDifference()
+        {			
+            var diagnostics = await TestHelper.GetDiagnosticsAsync(analyzer,
+				@"using System;
+
+class TestClass { 
+	    
+    public string StringProp { get; private set; }
+	public string StringProp2 { get; private set; }
+	public int IntField;
+
+	public TestClass(string stringArg, string stringProp2, int intArg)
+	{		
+        StringProp = stringArg;
+		IntField = intArg;
+		StringProp2 = stringProp2;
+	}
+}");
+
+            Assert.Collection(diagnostics.OrderBy(x => x.Location.GetLineSpan().StartLinePosition.Line),
+                d =>
+                {
+                    Assert.Equal(DiagnosticSeverity.Warning, d.Severity);
+                    Assert.Equal(10, d.Location.GetLineSpan().StartLinePosition.Line);
+                },
+				d =>
+	            {
+		            Assert.Equal(DiagnosticSeverity.Warning, d.Severity);
+		            Assert.Equal(11, d.Location.GetLineSpan().StartLinePosition.Line);
+	            });
+        }
+    }
+}
---
+++ src/ReviewHelp.Analyzers/Usage/ArgumentPropertyFieldNamingAnalyzer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace ReviewHelp.Analyzers.Usage
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class ArgumentPropertyFieldNamingAnalyzer : DiagnosticAnalyzer
+	{
+	    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } =
+	        ImmutableArray.Create(Descriptors.RH1004ArgumentNameDiffersFromPropertyAssignedTo);
+
+        public override void Initialize(AnalysisContext context)
+		{
+			context.EnableConcurrentExecution();
+			context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+
+			context.RegisterOperationBlockStartAction(ctx =>
+			{
+				if (!(ctx.OwningSymbol is IMethodSymbol m) || m.MethodKind != MethodKind.Constructor)
+				{
+					return;
+				}
+				
+				ctx.RegisterOperationAction(oc => AnalyzeOperation(oc, m), OperationKind.SimpleAssignment);			    
+            });		    
+		}
+
+	    private void AnalyzeOperation(OperationAnalysisContext context, IMethodSymbol methodSymbol)
+		{
+			if (!(context.Operation is IAssignmentOperation operation))
+			{
+				return;
+			}
+
+            var semanticModel = context.Compilation.GetSemanticModel(operation.Syntax.SyntaxTree);
+		    var sourceSymbol = semanticModel.GetSymbolInfo(operation.Value.Syntax);
+
+		    if (sourceSymbol.Symbol == null)
+		    {
+		        return;
+		    }
+
+            var matchingParameter = methodSymbol.Parameters.FirstOrDefault(x => sourceSymbol.Symbol.Name.Equals(x.Name, StringComparison.OrdinalIgnoreCase));
+
+		    if (matchingParameter == null)
+		    {
+		        return;
+		    }
+
+		    var targetSymbol = semanticModel.GetSymbolInfo(operation.Target.Syntax);
+
+		    if (targetSymbol.Symbol == null)
+		    {
+		        return;
+		    }
+
+		    if (!targetSymbol.Symbol.Name.Equals(sourceSymbol.Symbol.Name, StringComparison.OrdinalIgnoreCase))
+		    {
+				context.ReportDiagnostic(Diagnostic.Create(
+					SupportedDiagnostics[0],
+					operation.Syntax.GetLocation(), matchingParameter.Name, targetSymbol.Symbol.Name,
+						SymbolDisplayFormat.CSharpShortErrorMessageFormat.WithParameterOptions(
+							SymbolDisplayParameterOptions.None)));
+			}
+		
+		}
+	}
+}
